f1243b93c36b7c2f8129853170569237ee6ad0f3
Andy B <andybarnov@gmail.com>
2016-11-29 19:53:34 +0100

    add some comments

--- app/models/flight_package.rb
+++ app/models/flight_package.rb
@@ -3,6 +3,6 @@ class FlightPackage
   def initialize(args = {})
     @destination_city = args[:destination_city]
     @total = args[:total]
-    @trips = args[:trips]
+    @trips = args[:trips] # this is an array that holds two QPXTripOption instances
   end
 end
