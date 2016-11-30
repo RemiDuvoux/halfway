bda1cfb47fb9ff2f59b52357bc9a968df9d455fe
Andy B <andybarnov@gmail.com>
2016-11-30 10:10:43 +0100

    move RoundTrip to model

--- app/services/avion.rb
+++ app/services/avion.rb
@@ -2,30 +2,6 @@ require 'rest-client' # Make sure you have this gem!
 require 'json'
 
 module Avion
-
-  # An API agnostic version of QPXTripOption we use to refer inside of Offer
-  # A converter class. Use more initialization options with different APIs later
-  class RoundTrip
-    attr_reader :price, :destination_city, :destination_airport,
-                :origin_airport, :departure_time_there, :arrival_time_there,
-                :departure_time_back, :arrival_time_back, :currency, :carrier
-    def initialize(args={})
-      unless args[:qpx_trip_option].nil?
-        qpx = args[:qpx_trip_option]
-        @currency = qpx.currency
-        @price = qpx.price
-        @destination_city = qpx.destination_city
-        @destination_airport = qpx.destination_airport
-        @origin_airport = qpx.origin_airport
-        @departure_time_there = qpx.departure_time_there
-        @arrival_time_there = qpx.arrival_time_there
-        @departure_time_back = qpx.departure_time_back
-        @arrival_time_back = qpx.arrival_time_back
-        @carrier = qpx.carrier
-      end
-    end
-  end
-
   # Wraps an individual QPX response
   class QPXResponse
     attr_reader :trips
