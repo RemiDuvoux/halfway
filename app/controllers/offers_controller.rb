918426890c7793343edea38ba53b1894fc7e9344
Remi Duvoux <remi.duvoux@gmail.com>
2016-12-05 19:11:48 +0100

    filters done

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -36,6 +36,7 @@ class OffersController < ApplicationController
   end
 
   def index
+    @filters = params.to_hash.slice("origin_a", "date_there", "date_back", "origin_b")
     # do not cache the page to avoid caching waiting animation
     response.headers['Cache-Control'] = "no-cache, max-age=0, must-revalidate, no-store"
     # if there are no query params in URL or they don't make sense - send user to home page
@@ -71,10 +72,25 @@ class OffersController < ApplicationController
         }
         @offers.concat(Avion::SmartQPXAgent.new(info).obtain_offers)
       end
+
+      # filter by departure time if asked
+      if params["departure_time_there"].present? && params["departure_time_there"] != ""
+        @filters = @filters.merge(departure_time_there: params[:departure_time_there])
+        @offers = filter_by_departure_time(@offers)
+      end
+
+      #filter by arrival time if asked
+      if params["arrival_time_back"].present? && params["arrival_time_back"] != ""
+        @filters = @filters.merge(arrival_time_back: params[:arrival_time_back])
+        @offers = filter_by_arrival_time(@offers)
+      end
+
+      #and remove duplicate cities
+      @offers = @offers.uniq { |offer| offer.destination_city }
+
       # sort by total price
       @offers = @offers.sort_by { |offer| offer.total }
-      # and remove duplicate cities
-      @offers = @offers.uniq { |offer| offer.destination_city }
+
     else
       # we need this to be able to redirect user
       # to the page with same params in the url from the js in wait.html.erb
@@ -92,4 +108,49 @@ class OffersController < ApplicationController
     params[:origin_a].blank? || params[:origin_b].blank? || params[:date_there].blank? || params[:date_back].blank?
   end
 
+  def departure_time_choice
+    if params[:departure_time_there] == "earlybird"
+      return [5,10]
+    elsif params[:departure_time_there] == "morning"
+      return [9,12]
+    elsif params[:departure_time_there] == "afternoon"
+      return [11,18]
+    elsif params[:departure_time_there] == "afterwork"
+      return [17,24]
+    end
+  end
+
+  def arrival_time_choice
+    if params[:arrival_time_back] == "earlybird"
+      return [5,10]
+    elsif params[:arrival_time_back] == "morning"
+      return [9,12]
+    elsif params[:arrival_time_back] == "afternoon"
+      return [11,18]
+    elsif params[:arrival_time_back] == "evening"
+      return [17,24]
+    end
+  end
+
+  def departure_range
+    departure_as_date = Time.new(Time.parse(params[:date_there]).to_a[5],Time.parse(params[:date_there]).to_a[4],Time.parse(params[:date_there]).to_a[3])
+    (departure_as_date + departure_time_choice.first.hours .. departure_as_date + departure_time_choice.last.hours)
+  end
+
+  def arrival_range
+    arrival_as_date = Time.new(Time.parse(params[:date_back]).to_a[5],Time.parse(params[:date_back]).to_a[4],Time.parse(params[:date_back]).to_a[3])
+    (arrival_as_date + arrival_time_choice.first.hours .. arrival_as_date + arrival_time_choice.last.hours)
+  end
+
+  def filter_by_departure_time(offers)
+    offers.select { |offer|
+      departure_range.include?(offer.roundtrips.first.departure_time_there) && departure_range.include?(offer.roundtrips.last.departure_time_there)
+    }
+  end
+
+  def filter_by_arrival_time(offers)
+    offers.select { |offer|
+      arrival_range.include?(offer.roundtrips.first.arrival_time_back) && arrival_range.include?(offer.roundtrips.last.arrival_time_back)
+    }
+  end
 end
--- app/helpers/offers_helper.rb
+++ app/helpers/offers_helper.rb
@@ -1,3 +1,9 @@
 module OffersHelper
+  def departure_css_class_name(filter)
+    return "active" if filter == @filters[:departure_time_there]
+  end
 
+  def arrival_css_class_name(filter)
+    return "active" if filter == @filters[:arrival_time_back]
+  end
 end
