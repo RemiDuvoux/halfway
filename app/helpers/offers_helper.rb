b510cd0270e6e8272848ba7f8935efe8638175ef
Remi Duvoux <remi.duvoux@gmail.com>
2016-12-05 19:56:49 +0100

    without useless changes

--- app/helpers/offers_helper.rb
+++ app/helpers/offers_helper.rb
@@ -6,12 +6,4 @@ module OffersHelper
   def arrival_css_class_name(filter)
     return "active" if filter == @filters[:arrival_time_back]
   end
-
-  def date_a(offers)
-    offers.first.date_there.strftime("%b %d") if offers.present?
-  end
-
-  def date_b(offers)
-    @offers.first.date_back.strftime("%b %d") if offers.present?
-  end
 end
