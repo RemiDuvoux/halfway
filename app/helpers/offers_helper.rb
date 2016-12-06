ddfe2017f741d2617c053b74099caf1e648531a8
Remi Duvoux <remi.duvoux@gmail.com>
2016-12-06 17:57:54 +0100

    first draft

--- app/helpers/offers_helper.rb
+++ app/helpers/offers_helper.rb
@@ -6,4 +6,24 @@ module OffersHelper
   def arrival_css_class_name(filter)
     return "active" if filter == @filters[:arrival_time_back]
   end
+
+  def price_difference_styling(price1, price2)
+    if price2 > price1
+      return "#408000"
+    elsif price1 > price2
+      return "red"
+    else
+      return "black"
+    end
+  end
+
+  def price_difference_text(price1, price2)
+    if price2 > price1
+      "Cheaper by €#{(price2 - price1).round}"
+    elsif price1 > price2
+      "More expensive by €#{(price2 - price1).round}"
+    else
+      "The price is the same!"
+    end
+  end
 end
