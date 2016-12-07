9dfe60b02f015075b301e1a5e2afe49331238291
Andy B <andybarnov@gmail.com>
2016-12-07 15:47:03 +0100

    sshhhhs

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -20,8 +20,6 @@ class OffersController < ApplicationController
 
   def index
     airports = Constants::AIRPORTS.keys
-    # TODO: DONT PUSH!
-    airports = %w(PAR BER)
     date_there = params[:date_there]
     date_back = params[:date_back]
 
