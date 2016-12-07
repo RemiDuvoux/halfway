044116913fb8fdc3233846312712518d79320c5e
Andy B <andybarnov@gmail.com>
2016-12-07 12:46:08 +0100

    no requests to QPX will be made and user will be redirected to homepage if he tries to put bad search params directly into URL

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -20,6 +20,9 @@ class OffersController < ApplicationController
 
   def index
     airports = Constants::AIRPORTS.keys
+    # TODO: DONT PUSH WITH THIS!
+    airports = %w(PAR BER)
+
     date_there = params[:date_there]
     date_back = params[:date_back]
 
